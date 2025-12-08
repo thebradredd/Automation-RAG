28232ddb376c865fa3c4fa72e9ee145b8853296c
Chamod Basnayake <chamod.b@peopleshr.com>
2025-12-08 22:16:28 +0530

    Commit

--- test.py
+++ test.py
@@ -34,7 +34,7 @@ def load_documents():
 
 def split_documents(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=400,
+        chunk_size=800,
         chunk_overlap=80,
         length_function=len,
         is_separator_regex=False,
